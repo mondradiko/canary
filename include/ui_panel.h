934c39304dcacc6ae9af43b0dd666ce8cd89d0ee
marceline-cramer <cramermarceline@gmail.com>
2021-06-11 20:27:18 -0600

    Add ui_panel color-related functions

--- include/ui_panel.h
+++ include/ui_panel.h
@@ -19,11 +19,19 @@ mdo_result_t mdo_ui_panel_create (mdo_ui_panel_t **, const mdo_allocator_t *);
  */
 void mdo_ui_panel_delete (mdo_ui_panel_t *);
 
+/** @function mdo_ui_panel_set_color
+ */
+void mdo_ui_panel_set_color (mdo_ui_panel_t *, const float[4]);
+
+/** @function mdo_ui_panel_get_color
+ */
+void mdo_ui_panel_get_color (mdo_ui_panel_t *, float[4]);
+
 /*******************************************************************************
  * Scripting API
  ******************************************************************************/
 
 /** @function mdo_ui_panel_set_color_cb
  */
-wasm_trap_t *mdo_ui_panel_set_color_cb (void *, const wasm_val_t[],
-                                        wasm_val_t[]);
+wasm_trap_t *mdo_ui_panel_set_color_cb (void *, const wasm_val_vec_t *,
+                                        wasm_val_vec_t *);
