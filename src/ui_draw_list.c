30f4d8091f139248c1b304e1f7ba71fad0cd98d0
marceline-cramer <cramermarceline@gmail.com>
2021-06-15 16:54:29 -0600

    Add draw_list structure definition and data vectors

--- src/ui_draw_list.c
+++ src/ui_draw_list.c
@@ -3,16 +3,58 @@
 
 #include "ui_draw_list.h"
 
+struct mdo_ui_draw_list_s
+{
+  const mdo_allocator_t *alloc;
+
+  /* TODO(marceline-cramer): mdo-utils vector */
+  struct
+  {
+    mdo_ui_draw_vertex_t *vals;
+    size_t size;
+    size_t capacity;
+  } vertices;
+
+  struct
+  {
+    mdo_ui_draw_index_t *vals;
+    size_t size;
+    size_t capacity;
+  } indices;
+};
+
 mdo_result_t
 mdo_ui_draw_list_create (mdo_ui_draw_list_t **ui_draw,
                          const mdo_allocator_t *alloc)
 {
+  mdo_ui_draw_list_t *new_ui_draw = mdo_allocator_malloc (alloc, sizeof (mdo_ui_draw_list_t));
+  *ui_draw = new_ui_draw;
+
+  new_ui_draw->alloc = alloc;
+
+  new_ui_draw->vertices.vals = NULL;
+  new_ui_draw->vertices.size = 0;
+  new_ui_draw->vertices.capacity = 0;
+
+  new_ui_draw->indices.vals = NULL;
+  new_ui_draw->indices.size = 0;
+  new_ui_draw->indices.capacity = 0;
+
   return MDO_SUCCESS;
 }
 
 void
 mdo_ui_draw_list_delete (mdo_ui_draw_list_t *ui_draw)
 {
+  const mdo_allocator_t *alloc = ui_draw->alloc;
+
+  if (ui_draw->vertices.vals)
+    mdo_allocator_free (alloc, ui_draw->vertices.vals);
+
+  if (ui_draw->indices.vals)
+    mdo_allocator_free (alloc, ui_draw->indices.vals);
+
+  mdo_allocator_free (alloc, ui_draw);
 }
 
 mdo_ui_draw_index_t
