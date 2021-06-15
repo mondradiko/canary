6f337c9051d04201cfece6b0b389f15071d373fc
marceline-cramer <cramermarceline@gmail.com>
2021-06-15 00:53:17 -0600

    Fix ui_draw_list.c style

--- src/ui_draw_list.c
+++ src/ui_draw_list.c
@@ -4,11 +4,13 @@
 #include "ui_draw_list.h"
 
 mdo_result_t
-mdo_ui_draw_list_create (mdo_ui_draw_list_t **ui_draw, const mdo_allocator_t* alloc)
+mdo_ui_draw_list_create (mdo_ui_draw_list_t **ui_draw,
+                         const mdo_allocator_t *alloc)
 {
-	return MDO_SUCCESS;
+  return MDO_SUCCESS;
 }
 
-void mdo_ui_draw_list_delete (mdo_ui_draw_list_t *ui_draw)
+void
+mdo_ui_draw_list_delete (mdo_ui_draw_list_t *ui_draw)
 {
 }
