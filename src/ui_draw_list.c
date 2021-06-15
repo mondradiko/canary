87a0bd5b6115d3c41b98acd49b001081e87baf3f
marceline-cramer <cramermarceline@gmail.com>
2021-06-15 00:26:09 -0600

    Add ui_draw_list.c

---
+++ src/ui_draw_list.c
@@ -0,0 +1,14 @@
+/** @file ui_draw_list.c
+ */
+
+#include "ui_draw_list.h"
+
+mdo_result_t
+mdo_ui_draw_list_create (mdo_ui_draw_list_t **ui_draw, const mdo_allocator_t* alloc)
+{
+	return MDO_SUCCESS;
+}
+
+void mdo_ui_draw_list_delete (mdo_ui_draw_list_t *ui_draw)
+{
+}
