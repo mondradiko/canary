eed48ca117ad0a8fb2f1ee11077e0b3241b93f61
marceline-cramer <cramermarceline@gmail.com>
2021-06-15 16:41:51 -0600

    Initialize window surface and logical device
    main.c passes SDL_Window handle to vk_renderer_create so that it may
    create a surface

--- tests/harness/sdl/vk_renderer.cpp
+++ tests/harness/sdl/vk_renderer.cpp
@@ -3,6 +3,8 @@ extern "C"
 #include "vk_renderer.h"
 }
 
+#include <SDL2/SDL_vulkan.h>
+
 #include "VkBootstrap.h"
 
 struct vk_renderer_s
@@ -10,10 +12,13 @@ struct vk_renderer_s
   const mdo_allocator_t *alloc;
 
   vkb::Instance instance;
+  VkSurfaceKHR surface;
+  vkb::Device device;
+  vkb::Swapchain swapchain;
 };
 
 vk_renderer_t *
-vk_renderer_create (const mdo_allocator_t *alloc)
+vk_renderer_create (const mdo_allocator_t *alloc, SDL_Window *window)
 {
   vk_renderer_t *vk_renderer = static_cast<vk_renderer_t *> (
       mdo_allocator_malloc (alloc, sizeof (vk_renderer_t)));
@@ -33,6 +38,36 @@ vk_renderer_create (const mdo_allocator_t *alloc)
 
   vk_renderer->instance = instance_ret.value ();
 
+  if (SDL_Vulkan_CreateSurface (window, vk_renderer->instance.instance,
+                                &vk_renderer->surface)
+      != SDL_TRUE)
+    {
+      LOG_ERR ("failed to create SDL-Vulkan window surface: %s",
+               SDL_GetError ());
+      return NULL;
+    }
+
+  vkb::PhysicalDeviceSelector phys_device_selector (vk_renderer->instance);
+  auto phys_device_ret
+      = phys_device_selector.set_surface (vk_renderer->surface).select ();
+  if (!phys_device_ret)
+    {
+      LOG_ERR ("failed to find Vulkan physical device: %s",
+               phys_device_ret.error ().message ().c_str ());
+      return NULL;
+    }
+
+  vkb::DeviceBuilder device_builder{ phys_device_ret.value () };
+  auto device_ret = device_builder.build ();
+  if (!device_ret)
+    {
+      LOG_ERR ("failed to create Vulkan logical device: %s",
+               device_ret.error ().message ().c_str ());
+      return NULL;
+    }
+
+  vk_renderer->device = device_ret.value ();
+
   return vk_renderer;
 }
 
@@ -41,6 +76,8 @@ vk_renderer_delete (vk_renderer_t *vk_renderer)
 {
   const mdo_allocator_t *alloc = vk_renderer->alloc;
 
+  vkb::destroy_device (vk_renderer->device);
+  vkb::destroy_surface (vk_renderer->instance, vk_renderer->surface);
   vkb::destroy_instance (vk_renderer->instance);
 
   mdo_allocator_free (alloc, vk_renderer);
--- tests/harness/sdl/vk_renderer.h
+++ tests/harness/sdl/vk_renderer.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
 #include <mdo-utils/allocator.h>
 
 /** @typedef vk_renderer_t
  */
 typedef struct vk_renderer_s vk_renderer_t;
 
-vk_renderer_t *vk_renderer_create (const mdo_allocator_t *);
+vk_renderer_t *vk_renderer_create (const mdo_allocator_t *, SDL_Window *);
 
 void vk_renderer_delete (vk_renderer_t *);
