f3cfe66324cff8e315a07223b83bb3da0ff5e545
marceline-cramer <cramermarceline@gmail.com>
2021-06-15 17:42:27 -0600

    Implement ui_draw_vertex and _draw_triangle

--- src/ui_draw_list.c
+++ src/ui_draw_list.c
@@ -27,7 +27,8 @@ mdo_result_t
 mdo_ui_draw_list_create (mdo_ui_draw_list_t **ui_draw,
                          const mdo_allocator_t *alloc)
 {
-  mdo_ui_draw_list_t *new_ui_draw = mdo_allocator_malloc (alloc, sizeof (mdo_ui_draw_list_t));
+  mdo_ui_draw_list_t *new_ui_draw
+      = mdo_allocator_malloc (alloc, sizeof (mdo_ui_draw_list_t));
   *ui_draw = new_ui_draw;
 
   new_ui_draw->alloc = alloc;
@@ -61,11 +62,53 @@ mdo_ui_draw_index_t
 mdo_ui_draw_vertex (mdo_ui_draw_list_t *ui_draw,
                     const mdo_ui_draw_vertex_t *vertex)
 {
-  return 0;
+  const mdo_allocator_t *alloc = ui_draw->alloc;
+
+  mdo_ui_draw_index_t index = ui_draw->vertices.size++;
+
+  if (ui_draw->vertices.capacity == 0)
+    {
+      ui_draw->vertices.capacity = 1024;
+      ui_draw->vertices.vals = mdo_allocator_calloc (
+          alloc, ui_draw->vertices.capacity, sizeof (mdo_ui_draw_vertex_t));
+    }
+  else if (index >= ui_draw->vertices.capacity)
+    {
+      ui_draw->vertices.capacity = ui_draw->vertices.capacity << 1;
+      ui_draw->vertices.vals = mdo_allocator_realloc (
+          alloc, ui_draw->vertices.vals,
+          sizeof (mdo_ui_draw_vertex_t) * ui_draw->vertices.capacity);
+    }
+
+  return index;
 }
 
 void
 mdo_ui_draw_triangle (mdo_ui_draw_list_t *ui_draw, mdo_ui_draw_index_t vertex1,
                       mdo_ui_draw_index_t vertex2, mdo_ui_draw_index_t vertex3)
 {
+  const mdo_allocator_t *alloc = ui_draw->alloc;
+
+  size_t index_offset = ui_draw->indices.size;
+  ui_draw->indices.size += 3;
+
+  if (ui_draw->indices.capacity == 0)
+    {
+      ui_draw->indices.capacity = 1536;
+      ui_draw->indices.vals = mdo_allocator_calloc (
+          alloc, ui_draw->indices.capacity, sizeof (mdo_ui_draw_index_t));
+    }
+  else if (ui_draw->indices.size > ui_draw->indices.capacity)
+    {
+      ui_draw->indices.capacity = ui_draw->indices.capacity << 1;
+      ui_draw->indices.vals = mdo_allocator_realloc (
+          alloc, ui_draw->indices.vals,
+          sizeof (mdo_ui_draw_index_t) * ui_draw->indices.capacity);
+    }
+
+  mdo_ui_draw_index_t *indices = &ui_draw->indices.vals[index_offset];
+
+  indices[0] = vertex1;
+  indices[1] = vertex2;
+  indices[2] = vertex3;
 }
