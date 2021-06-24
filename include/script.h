6e9de0001360fc501b553139aa2750f207756716
marceline-cramer <cramermarceline@gmail.com>
2021-06-24 01:03:09 -0600

    Declare script_on_input and input event enum

--- include/script.h
+++ include/script.h
@@ -18,6 +18,16 @@ typedef struct canary_script_s canary_script_t;
  */
 typedef uint32_t canary_panel_key_t;
 
+/** @typedef canary_input_event_t
+ */
+typedef enum
+{
+  CANARY_HOVER,
+  CANARY_SELECT,
+  CANARY_DRAG,
+  CANARY_DESELECT,
+} canary_input_event_t;
+
 /** @function canary_script_create
  * @param script
  * @param alloc
@@ -45,6 +55,12 @@ mdo_result_t canary_script_load (canary_script_t *, const char *);
  */
 wasm_trap_t *canary_script_new_trap (canary_script_t *, const char *);
 
+/** @function canary_script_update
+ * @param script
+ * @param dt
+ */
+void canary_script_update (canary_script_t *, float);
+
 /** @function canary_script_bind_panel
  * @param script
  * @param panel
@@ -67,3 +83,12 @@ void canary_script_unbind_panel (canary_script_t *, canary_panel_key_t);
  */
 canary_panel_t *canary_script_lookup_panel (canary_script_t *,
                                             canary_panel_key_t);
+
+/** @function canary_script_on_input
+ * @param script
+ * @param panel_key
+ * @param event_type
+ * @param coords
+ */
+void canary_script_on_input (canary_script_t *, canary_panel_key_t,
+                             canary_input_event_t, float[2]);
