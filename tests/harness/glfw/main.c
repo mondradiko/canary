ffd48b8fb81248bc2c4ebed802cddd540b50ddcd
marceline-cramer <cramermarceline@gmail.com>
2021-06-25 01:49:37 -0600

    Refactor glfw/main.c

--- tests/harness/glfw/main.c
+++ tests/harness/glfw/main.c
@@ -3,34 +3,57 @@
 #define GLFW_INCLUDE_ES2
 #include "GLFW/glfw3.h"
 
-int log_error(const char *message) {
-  fprintf(stderr, "%s\n", message);
+#include <mdo-utils/result.h>
+
+static int
+log_error (const char *message)
+{
+  fprintf (stderr, "%s\n", message);
   return -1;
 }
 
-int main() {
-  if (!glfwInit())
-    return log_error("failed to init glfw");
+int
+run_harness (const char *filename)
+{
+  if (!glfwInit ())
+    return log_error ("failed to init glfw");
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint (GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 0);
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "ES Test", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow (800, 600, "ES Test", NULL, NULL);
   if (!window)
-    return log_error("failed to create window");
+    return log_error ("failed to create window");
 
-  glfwMakeContextCurrent(window);
+  glfwMakeContextCurrent (window);
 
-  printf("GL_VERSION  : %s\n", glGetString(GL_VERSION));
-  printf("GL_RENDERER : %s\n", glGetString(GL_RENDERER));
+  printf ("GL_VERSION  : %s\n", glGetString (GL_VERSION));
+  printf ("GL_RENDERER : %s\n", glGetString (GL_RENDERER));
 
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-  }
+  while (!glfwWindowShouldClose (window))
+    {
+      glfwPollEvents ();
+      glClear (GL_COLOR_BUFFER_BIT);
+      glfwSwapBuffers (window);
+    }
 
-  glfwTerminate();
+  glfwTerminate ();
   return 0;
+}
+
+int
+main (int argc, const char *argv[])
+{
+  if (argc != 2)
+    {
+      fprintf (stderr, "Usage:\n  %s [ui_script.wasm]\n", argv[0]);
+      return 1;
+    }
+
+  const char *filename = argv[1];
+  int result = run_harness (filename);
+  mdo_result_cleanup ();
+
+  return result;
 }
